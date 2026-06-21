d607aab5a689808e968be3cf5ac40008cb87b79e
Muhammad Usama Ashraf <usamaashraf82@live.com>
2026-06-21 14:45:49 +0500

    feat: TypeScript types for all Sanity document shapes

---
+++ src/types/index.ts
@@ -0,0 +1,71 @@
+// src/types/index.ts
+
+export interface HeroStat {
+  num: string
+  label: string
+}
+
+export interface SiteSettings {
+  heroTitle: string
+  heroTitleHighlight: string
+  heroDesc: string
+  heroStats: HeroStat[]
+  trustLogos: Array<{ name: string }>
+  ctaTitle: string
+  ctaSubtitle: string
+}
+
+export interface Service {
+  _id: string
+  title: string
+  icon: string
+  description: string
+  bullets: string[]
+  order: number
+}
+
+export interface TeamMember {
+  _id: string
+  name: string
+  role: string
+  bio: string
+  credentials: string
+  initials: string
+  gradientFrom: string
+  gradientTo: string
+  order: number
+}
+
+export interface Testimonial {
+  _id: string
+  quote: string
+  authorName: string
+  authorRole: string
+  initials: string
+  order: number
+}
+
+export interface CaseResult {
+  num: string
+  label: string
+}
+
+export interface CaseStudy {
+  _id: string
+  company: string
+  industry: string
+  tag: string
+  challenge: string
+  results: CaseResult[]
+  order: number
+}
+
+export interface Post {
+  _id: string
+  title: string
+  excerpt: string
+  category: string
+  author: { name: string }
+  publishedAt: string
+  readTime: number
+}
