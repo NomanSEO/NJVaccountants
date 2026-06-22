a7fef53591c1147c28cf846c3053907cb06b6613
NomanSEO <nomanj.ajs@gmail.com>
2026-06-22 12:01:12 +0500

    update

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,6 +6,7 @@ import { schemaTypes } from './src/sanity/schema'
 export default defineConfig({
   name: 'pinnacle',
   title: 'Pinnacle Advisory',
+  basePath: '/studio',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
   plugins: [structureTool()],
